d591c8ec1b688b745309fcde7c68ba8398ace036
kitety <aizaizuori@foxmail.com>
2018-10-27 23:04:52 +0800

    中间件basic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
 // import { increment, decrement } from './actions';
 // 引入顶级组件
@@ -10,8 +10,34 @@ import { Provider } from 'react-redux';
 //index其实可以省略
 // 传递过来的是方法,因此要执行
 
+// 函数的嵌套
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action)
+  console.log('next state', store.getState());
+  return result;
+}
+const err = store => next => action => {
+  try {
+    next(action);
+    // 进入下一个中间件,但是没有下一个中间件了,就进入reducer
+  } catch (e) {
+    console.error('err' + e);
+  }
+}
+// es5版本
+// const logger1 = function (store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log('dispatching', action);
+//       let result = next(action)
+//       console.log('next state', store.getState());
+//       return result;
+//     }
+//   }
+// }
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, {}, applyMiddleware(logger, err))
 // store.subscribe(() => console.log("State update!", store.getState()))
 // store.dispatch({
 //   type: 'INCREMENT'
--- src/reducer/counter.js
+++ src/reducer/counter.js
@@ -2,6 +2,7 @@
 const counter = (state = 1, action = {}) => {
   switch (action.type) {
     case 'INCREMENT':
+      // throw new Error('1');
       return state + 1;
     case 'DECREMENT':
       return state - 1;
