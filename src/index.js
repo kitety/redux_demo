c7f6e5fea336555fea15e52ec89cb11e47a51077
kitety <aizaizuori@foxmail.com>
2018-10-27 23:09:40 +0800

    redux-logger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import rootReducer from './reducer'
 // import { increment, decrement } from './actions';
 // 引入顶级组件
@@ -11,20 +12,20 @@ import { Provider } from 'react-redux';
 // 传递过来的是方法,因此要执行
 
 // 函数的嵌套
-const logger = store => next => action => {
-  console.log('dispatching', action);
-  let result = next(action)
-  console.log('next state', store.getState());
-  return result;
-}
-const err = store => next => action => {
-  try {
-    next(action);
-    // 进入下一个中间件,但是没有下一个中间件了,就进入reducer
-  } catch (e) {
-    console.error('err' + e);
-  }
-}
+// const loggerOwn = store => next => action => {
+//   console.log('dispatching', action);
+//   let result = next(action)
+//   console.log('next state', store.getState());
+//   return result;
+// }
+// const err = store => next => action => {
+//   try {
+//     next(action);
+//     // 进入下一个中间件,但是没有下一个中间件了,就进入reducer
+//   } catch (e) {
+//     console.error('err' + e);
+//   }
+// }
 // es5版本
 // const logger1 = function (store) {
 //   return function (next) {
@@ -37,7 +38,7 @@ const err = store => next => action => {
 //   }
 // }
 
-const store = createStore(rootReducer, {}, applyMiddleware(logger, err))
+const store = createStore(rootReducer, {}, applyMiddleware(logger))
 // store.subscribe(() => console.log("State update!", store.getState()))
 // store.dispatch({
 //   type: 'INCREMENT'
