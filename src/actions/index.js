502c228c43b04e30ae20f45d5a97510d54d9ef8a
kitety <aizaizuori@foxmail.com>
2018-10-28 22:15:20 +0800

    请求的三种状态

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT, FETCH_USER_SUCCESS } from '../constants/index';
+import { INCREMENT, DECREMENT, FETCH_USER_SUCCESS, FETCH_USER_REQUEST, FETCH_USER_FAILURE } from '../constants/index';
 import axios from 'axios';
 // import * as types from '../constants/index';
 //注意引用的形式 types.INCREMENT
@@ -14,23 +14,35 @@ export const decrement = () => {
 }
 export const getUser = () => {
   return dispatch => {
+    dispatch(fetch_user_requset())
     axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=d5025dee4eca4159893374b378307cdc')
       .then(function (response) {
         // handle success
-        console.log(response);
-        let a=~~(Math.random()*20);
-        dispatch(fetchUser(response.data.articles[a]))
+        let a = ~~(Math.random() * 20);
+        dispatch(fetch_uesr_success(response.data.articles[a]))
       })
       .catch(function (error) {
+        dispatch(fetch_user_failure(error.response.data.message))
         // handle error
-        console.log(error);
       })
   }
 }
-export const fetchUser = (user) => {
+// 返回对象的函数,因此要执行
+export const fetch_uesr_success = (user) => {
   return {
     type: FETCH_USER_SUCCESS,
     user
+  }
+}
+export const fetch_user_requset = () => {
+  return {
+    type: FETCH_USER_REQUEST
+  }
+}
+export const fetch_user_failure = (error) => {
+  return {
+    type: FETCH_USER_FAILURE,
+    error
   }
 }
 // 注意 暴露的是函数 要执行
--- src/components/user.js
+++ src/components/user.js
@@ -4,10 +4,19 @@ import { getUser } from '../actions/index'
 
 class User extends Component {
   render() {
-    const { getUser, user } = this.props;
+    const { getUser } = this.props;
+    const { error, isFetching, user } = this.props.user;
+    let data;
+    if (error) {
+      data = error
+    } else if (isFetching) {
+      data = "Loading..."
+    } else {
+      data = user.title
+    }
     return (
       <div >
-        <h1 className="jumpbotron-heading text-center">{user.author}</h1>
+        <h1 className="jumpbotron-heading text-center">{data}</h1>
         <p className="text-center">
           <button onClick={() => { getUser() }} className="btn btn-success mr-2">GET RANDOM USER</button>
         </p>
